87491946e5ef6b4685aaddb2916fa437f9ac500f
Nicole Villalpando <75922885+nmvilla15@users.noreply.github.com>
2021-08-24 15:57:27 -0500

    added date/time to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,14 @@
 class Message < ApplicationRecord
-  belongs_to :conversation
-  belongs_to :user
+  belongs_to :conversation, dependent: :destroy
+  belongs_to :user, dependent: :destroy  
 
 #validates_presence_of :body, :conversation_id, :user_id
   validates :body, presence: true
   validates :conversation_id, presence: true
   validates :user_id, presence: true
+
+  def sent_time
+    created_at.strftime("%m/%d/%y at %l:%M %p")
+  end
   
 end
