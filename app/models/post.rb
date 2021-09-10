01dc96d51afc2d0ffcd2afcc6c3b4ea59e1f73a7
Anastasiia Bateman <Anastasiia.bateman@gmail.com>
2021-09-10 16:21:38 -0400

    make search case insensitive

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
 
     def self.search(search)
         if search 
-            where(" title LIKE ?", "%#{search}%")
+            where(" lower(title)  LIKE ?", "%#{search}%")
         else
             all
         end
