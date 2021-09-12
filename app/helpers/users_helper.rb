e3138598dd0d79c7c2d849a1c8d8c253fb562a5c
Nicole Villalpando <75922885+nmvilla15@users.noreply.github.com>
2021-09-12 15:38:00 -0500

    added gravatar option if no profile pic uploaded

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+    def avatar_for(user)
+        if user.avatar.attached?
+            image_tag(user.avatar, style: 'width:5%') 
+        else
+            gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+            gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+            image_tag(gravatar_url, alt: user.username, class: "gravatar")
+        end
+    end
 end
