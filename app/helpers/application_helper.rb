aa88d2bbdc9a073068ddbb7103f70886cfb03385
Anastasiia Bateman <Anastasiia.bateman@gmail.com>
2021-09-20 19:21:58 -0400

    Created popup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
 
+    def bootstrap_class_for flash_type
+        { alert: "alert-danger", notice: "alert-success" }.stringify_keys[flash_type.to_s] || flash_type.to_s
+    end
+    
+    def flash_messages(opts = {})
+        flash.each do |msg_type, message|
+          concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert") do 
+                  concat message 
+                end)
+        end
+        nil
+    end
 end
