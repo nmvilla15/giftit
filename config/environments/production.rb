33212b6c10a26d53e5b39a3201ed7bfcc29a434c
Nicole Villalpando <75922885+nmvilla15@users.noreply.github.com>
2021-10-02 21:21:00 -0500

    updated production eviroment to fix errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,12 +27,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
+  config.assets.compile = true
+  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
