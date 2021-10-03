a626ac110a9263cebab25ac18ae0984740cac5db
Nicole Villalpando <75922885+nmvilla15@users.noreply.github.com>
2021-10-02 20:36:19 -0500

    updated production for herouk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,11 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
